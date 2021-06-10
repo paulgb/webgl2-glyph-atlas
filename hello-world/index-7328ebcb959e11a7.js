ee5c206b0ae884f49faef806bf224978821e3625
paulgb <paulgb@gmail.com>
2021-06-10 13:37:01 +0000

    Deploying to gh-pages from @ paulgb/webgl2-glyph-atlas@93c60d3b6031eef3e8d8b033c21a1a571567cb0d 🚀

--- hello-world/index-7328ebcb959e11a7.js
+++ hello-world/index-7328ebcb959e11a7.js
@@ -219,7 +219,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-89758be4099085_bg.wasm', import.meta.url);
+        input = new URL('index-7328ebcb959e11a7_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
